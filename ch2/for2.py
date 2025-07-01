34acd7722386a25b04add818da2336a83d811746
corcorasgit <corcoras.git@protonmail.com>
2025-07-01 13:21:07 -0700

    functions

---
+++ ch2/for2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+print("My name is:")
+i = 0
+while i < 5:
+    print ("Jimmy Five Times " + str(i))
+    i = i + 1
+
+print("*" * 20)
+
+print("My name is ~~:")
+for i in range (0, 10, 2):
+    print("Jimmy five times " + str(i))
+
+
+print("*" * 20)
