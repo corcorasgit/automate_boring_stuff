f760c6e6082f03fad8874e975c36351706ba5829
corcorasgit <corcoras.git@protonmail.com>
2025-07-03 10:56:49 -0700

    guess my number program

---
+++ ch5/guess.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import random
+# This is a guess the number game.
+print("Hello. What is you name?")
+name = input()
+secretNumber = random.randint(1, 20)
+print("Well, " + name + ", I am thinging of a number between 1 and 20")
+
+# Ask the player to guess 6 times.
+for  guessesTaken in range(1, 7):
+    print("Take a guess")
+    guess = int(input())
+    if guess < secretNumber:
+        print("Your guess is too low.")
+    elif guess > secretNumber:
+        print("Your guess is too high.")
+    else:
+        break # This condition is the correct guess!
+
+if guess == secretNumber:
+    print("Good job,  " + name + " ! You guessed my number in ")
+else:
+    print("Nope. The number I was thinking of was " + str(secretNumber))
