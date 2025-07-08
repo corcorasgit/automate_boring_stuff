90e027f932bce902ce232f6562761e530ac3dd4b
corcorasgit <corcoras.git@protonmail.com>
2025-07-08 09:00:31 -0700

    webscraping

---
+++ ch13/getRoeo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import requests
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+res.status_code
+
+
+playFile = open('RomeoAndJuliet2.txt', 'wb'):
+    playFile.write(chunk)
+
+playFile.close()
+
+
+  
